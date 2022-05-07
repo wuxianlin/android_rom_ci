db3c8888e4e278527c3abb75b1f6811b9da709a9
wuxianlin <wuxianlinwxl@gmail.com>
2022-05-07 23:52:06 +0800

    update mariana-trench

--- test-mariana-trench.sh
+++ test-mariana-trench.sh
@@ -9,9 +9,20 @@ rm -r out/rom
 ROM=out/rom-deodexed
 for apk in `find $ROM -name *.apk`;do
 	echo "---- start test $apk ----"
+	apkfolder="$(dirname $apk)"
+	filenum=`find $apkfolder -name *.apk -o -name *.jar | wc -l`
+	folder=${apkfolder#$ROM*}
+	apknametmp="$(basename $apk)"
+	apkname=${apknametmp%.*}
+	if [ "${apkfolder##*/}" != "$apkname" ] || [ $filenum -gt 1 ];then
+		outfolder=$folder/$apkname
+	else
+		outfolder=$folder
+	fi
+	mkdir -p test/$outfolder
 	if unzip -v $apk | grep " classes.dex" >/dev/null; then
 		echo "---- $apk has dex file ----"
-		mariana-trench --system-jar-configuration-path=$ANDROID_HOME/platforms/android-30/android.jar --apk-path=$apk
+		mariana-trench --system-jar-configuration-path=$ANDROID_HOME/platforms/android-30/android.jar --apk-path=$apk --output-directory test/$outfolder
 	        echo $apk >> result.list
 	fi
 	echo "---- test $apk done ----"
